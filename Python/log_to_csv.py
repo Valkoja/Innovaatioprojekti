b54c8b775918a0bdebb475cfac7244cda2b5ad7b
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2019-01-16 00:32:16 +0200

    made log to csv converter

--- Python/log_to_csv.py
+++ Python/log_to_csv.py
@@ -2,22 +2,27 @@
 """
 Created on Tue Jan 15 13:27:27 2019
 
-@author: kk426
+@author: rohtua
 """
 import os
 
 def main():
-    file_path = os.getcwd() + '\\file_to_csv.txt'
-    
-    f=open(file_path,"r+")
-    
+    file_path = os.getcwd() + '\\logfile.log'
+    file_csv = os.getcwd() + '\\logfile.csv'
+    f=open(file_path,"r")
     f1 = f.readlines()
-    
     for x in f1:
-        #line here
+        y=x
         if x.find("***")==-1:
-            x.replace(" Rx 1 ",",Rx,1,")
-            
-
+            x = x.replace(" ",",",6)
+        elif x.find("***<Time>")==0:
+            x = x.replace("><",">,<",6)
+        x = x.replace("***","")
+        f1[f1.index(y)] = x
+    f1[5] = f1[len(f1)-3]
+    f.close()
+    f=open(file_csv,"w+")
+    f.writelines(f1)
+    f.close()
 if __name__=="__main__":
     main()
