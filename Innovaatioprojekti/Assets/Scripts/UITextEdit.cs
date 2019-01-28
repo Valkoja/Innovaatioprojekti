deab9cb7fbae90c5891d9ed9b6c1a788e45d03f6
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2019-01-28 12:38:21 +0200

    Editable text to UI

---
+++ Innovaatioprojekti/Assets/Scripts/UITextEdit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextEdit : MonoBehaviour
+{
+    public double xValue;
+    public double yValue;
+
+    public Text xText;
+    public Text yText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xValue = 0.0;
+        yValue = 0.0;
+        SetTextWith(xValue, yValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTextWith(xValue, yValue);
+    }
+
+    void SetTextWith(double x, double y) {
+        xText.text = "X: "+x.ToString();
+        yText.text = "Y: "+y.ToString();
+    }
+}
