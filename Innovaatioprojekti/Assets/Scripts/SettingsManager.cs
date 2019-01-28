3cb6d305c5c9f7c0b8d7cdc2f5fa877fc15a3863
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2019-01-28 16:09:48 +0200

    moved scripts around, created settings manager

---
+++ Innovaatioprojekti/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    public bool CoordVisibility;
+    public bool FPSVisibility;
+    public string IPaddress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoordVisibility = true;
+        FPSVisibility = true;
+        IPaddress = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
