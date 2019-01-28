414f84fb8fd3a7011196591ae2c0791880d26f14
Teemu Uusi-Rasi <teemu.uusi-rasi@eng.tamk.fi>
2019-01-28 11:18:25 +0200

    Ground, skybox, lighting and movement tested or done

---
+++ Innovaatioprojekti/Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+	public Transform target;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform);
+		transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
