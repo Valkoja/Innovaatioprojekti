0671bcaada1113c2e4c06945f7219808bfd3d442
Iiro Valkoja <iiro.valkoja@gmail.com>
2019-02-15 01:52:46 +0200

    I know python, honest..

--- Python/Server/gui/ModelBridge.py
+++ Python/Server/gui/ModelBridge.py
@@ -8,20 +8,20 @@ class ModelBridge(QObject):
     @pyqtSlot(result=float)
     def getBoomAngle(self):
         if 'main_boom' in self.stateObject.getState()['angles']:
-        return self.stateObject.getState()['angles']['main_boom'] / 10
-        else
+            return self.stateObject.getState()['angles']['main_boom'] / 10
+        else:
             return 60
 
     @pyqtSlot(result=float)
     def getDiggingArmAngle(self):
         if 'digging_arm' in self.stateObject.getState()['angles']:
             return self.stateObject.getState()['angles']['digging_arm'] / 10
-        else
+        else:
             return 40
 
     @pyqtSlot(result=float)
     def getBucketAngle(self):
         if 'bucket' in self.stateObject.getState()['angles']:
             return self.stateObject.getState()['angles']['bucket'] / 10
-        else
+        else:
             return 20
