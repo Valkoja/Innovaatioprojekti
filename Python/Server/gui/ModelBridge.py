9899bf30ebd6e8810029e7326865e7134eb6389e
Iiro Valkoja <iiro.valkoja@gmail.com>
2019-02-15 01:51:59 +0200

    Duct tape had wrong type of indentation

--- Python/Server/gui/ModelBridge.py
+++ Python/Server/gui/ModelBridge.py
@@ -1,6 +1,5 @@
 from PyQt5.QtCore import QObject, pyqtSlot
 
-
 class ModelBridge(QObject):
     def __init__(self, stateObject):
         super().__init__()
@@ -8,21 +7,21 @@ class ModelBridge(QObject):
 
     @pyqtSlot(result=float)
     def getBoomAngle(self):
-		if 'main_boom' in self.stateObject.getState()['angles']:
-			return self.stateObject.getState()['angles']['main_boom'] / 10
-		else
-			return 60
+        if 'main_boom' in self.stateObject.getState()['angles']:
+        return self.stateObject.getState()['angles']['main_boom'] / 10
+        else
+            return 60
 
     @pyqtSlot(result=float)
     def getDiggingArmAngle(self):
-		if 'digging_arm' in self.stateObject.getState()['angles']:
-			return self.stateObject.getState()['angles']['digging_arm'] / 10
-		else
-			return 40
+        if 'digging_arm' in self.stateObject.getState()['angles']:
+            return self.stateObject.getState()['angles']['digging_arm'] / 10
+        else
+            return 40
 
     @pyqtSlot(result=float)
     def getBucketAngle(self):
-		if 'bucket' in self.stateObject.getState()['angles']:
-			return self.stateObject.getState()['angles']['bucket'] / 10
-		else
-			return 20
+        if 'bucket' in self.stateObject.getState()['angles']:
+            return self.stateObject.getState()['angles']['bucket'] / 10
+        else
+            return 20
