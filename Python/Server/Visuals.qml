07a9b5f46d1745ea17168852c48b6ddb7f0bc97d
Iiro Valkoja <iiro.valkoja@gmail.com>
2019-02-17 22:38:10 +0200

    Reorganize and rename, added arm & bucket to code pending svg files

--- Python/Server/Visuals.qml
+++ Python/Server/Visuals.qml
@@ -2,18 +2,24 @@ import QtQuick 2.9
 import QtQuick.Controls 2.2
 import QtQuick.Controls.Material 2.0
 import QtQuick.Layouts 1.3
-import SVGView 1.0
+import SVGElement 1.0
 
 ColumnLayout {
+    // Miksi tämä korjaa tilanteen?
+    anchors.fill: parent
+
     Header {
         title: "Information"
         width: parent.parent.width
     }
 
-    SVGView {
-        id: visualizationCanvas 
-        Layout.preferredWidth: 700
-        Layout.preferredHeight: 700
+    SVGElement {
+        id: svgViewport 
+        // Layout.preferredWidth: 600
+        // Layout.preferredHeight: 600
+        Layout.fillWidth: true
+        Layout.fillHeight: true
+        boomAngle: modelWrapper.mainBoomAngle
 
 /*
         renderStrategy: Canvas.Threaded
---
+++ Python/Server/gui/SVGElement.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtCore import QObject, pyqtProperty
+from PyQt5.QtQuick import QQuickPaintedItem
+from PyQt5.QtSvg import QSvgRenderer
+
+import math, svgutils
+
+class SVGElement(QQuickPaintedItem):
+    def __init__(self, parent = None):
+        super().__init__(self, parent)
+        self.setOpaquePainting(False)
+
+        self._boomSVG = svgutils.compose.SVG('./gui/svg/boom.svg')
+        self._boomAngle = 0
+        self._armSVG = svgutils.compose.SVG('./gui/svg/boom.svg')
+        self._armAngle = 0
+        self._bucketSVG = svgutils.compose.SVG('./gui/svg/boom.svg')
+        self._bucketAngle = 0
+
+
+    def calculateX(self, angle, length):
+        x = math.floor(math.fabs(math.cos(math.radians(angle)) * length))
+
+        if -90 <= angle <= 90:
+            x = x * -1
+        
+        return x
+
+
+    def calculateY(self, angle, length):
+        y = math.floor(math.fabs(math.sin(math.radians(angle)) * length))
+        
+        if 0 <= angle <= 180:
+            y = y * -1
+        
+        return y
+
+
+    def resizeEvent(self, event):
+        self.update()
+
+
+    def paint(self, painter):
+        # qp = painter
+
+        # Puomin alkupisteen koordinaatit
+        boomX = 900
+        boomY = 900
+
+        boomSVG = self._boomSVG
+        boomSVG.rotate(self._boomAngle, 500, 500)
+        boomSVG.move(boomX - 500, boomY - 500)
+
+        # Toinen puomi
+        armX = boomX + self.calculateX(self._boomAngle, 385)
+        armY = boomY + self.calculateY(self._boomAngle, 385)
+
+        armSVG = self._armSVG
+        armSVG.rotate(self._armAngle, 500, 500)
+        armSVG.move(armX - 500, armY - 500)
+
+        # Kauha
+        bucketX = armX + self.calculateX(self._boomAngle + self._armAngle, 176)
+        bucketY = armY + self.calculateY(self._boomAngle + self._armAngle, 176)
+
+        bucketSVG = self._bucketSVG
+        bucketSVG.rotate(self._bucketAngle, 500, 500)
+        bucketSVG.move(bucketX - 500, bucketY - 500)
+
+        # Yhdistetään
+        compose = svgutils.compose.Figure('1000px', '1000px', boomSVG)
+        svg = QSvgRenderer(compose.scale(1).tostr())
+        svg.render(painter)
+
+
+
+    '''
+        # Vanhoja?
+        self.setOpaquePainting(False)
+        self.setTextureSize = QSize(600, 600)
+
+        # Elementin koko
+        print(str(self.width()))
+        print(str(self.height()))
+    '''
+
+    @pyqtProperty(int)
+    def boomAngle(self):
+        return self._boomAngle
+
+    @boomAngle.setter
+    def boomAngle(self, boomAngle):
+        self._boomAngle = boomAngle
+
+    @pyqtProperty(int)
+    def armAngle(self):
+        return self._armAngle
+
+    @armAngle.setter
+    def armAngle(self, armAngle):
+        self._armAngle = armAngle
+
+    @pyqtProperty(int)
+    def bucketAngle(self):
+        return self._bucketAngle
+
+    @bucketAngle.setter
+    def bucketAngle(self, bucketAngle):
+        self._bucketAngle = bucketAngle
