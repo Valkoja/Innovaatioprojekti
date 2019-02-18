48403282ce1450bde1db106a241af047b03b0a49
Tomi Heino <tomi.heino@eng.tamk.fi>
2019-02-18 20:41:32 +0200

    Add scripts for model values

---
+++ Innovaatioprojekti/Assets/Scripts/Model/MachineState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Holds values in irl values
+public class MachineState : MonoBehaviour
+{
+    public float mainBoomAngle = 0f;
+    public float diggingArmAngle = 0f;
+    public float bucketAngle = 0f;
+
+    void Start()
+    {
+        // Reasonable initial state
+        this.mainBoomAngle = 45;
+        this.diggingArmAngle = -120;
+        this.bucketAngle = -30;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void consumeMessage(MachineStateMessage message)
+    {
+        if(message.quaternions.main_boom_orientation != null) {
+            var quat = message.quaternions.main_boom_orientation;
+            this.mainBoomAngle = xAngleFromRawQuaternion(quat);
+        }
+        if(message.quaternions.digging_arm_orientation != null) {
+            var quat = message.quaternions.digging_arm_orientation;
+            this.diggingArmAngle = xAngleFromRawQuaternion(quat);
+        }
+        if(message.quaternions.bucket_orientation != null) {
+            var quat = message.quaternions.bucket_orientation;
+            this.bucketAngle = xAngleFromRawQuaternion(quat);
+        }
+    }
+
+    public float xAngleFromRawQuaternion(RawQuaternion quaternion) {
+        return new Quaternion(quaternion.x, quaternion.y, quaternion.z, quaternion.w).eulerAngles.x;
+    }
+}
