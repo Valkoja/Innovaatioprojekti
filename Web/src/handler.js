54f32a038f972602015f14c98478315d2762649c
Iiro Valkoja <Iiro.Valkoja@Gmail.com>
2019-04-20 20:34:48 +0300

    Implemented websocket tutorial for handler

--- Web/src/handler.js
+++ Web/src/handler.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Websocket from 'react-websocket';
 
 class Handler extends React.Component
 {
@@ -6,9 +7,18 @@ class Handler extends React.Component
     //     super(props);
     // }
 
+    handleData(data) {
+        let result = JSON.parse(data);
+        // this.setState({count: this.state.count + result.movement});
+        console.log(data);
+    }
+
     render() {
         return (
-            <div>Handler</div>
+            <div>
+                Handler
+                <Websocket url='ws://localhost:8888/live/product/12345/' onMessage={this.handleData.bind(this)} />
+            </div>
         );
     }
 }
