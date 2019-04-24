ed9714ae23964be205203f64ca071fe8359b008b
Iiro Valkoja <Iiro.Valkoja@Gmail.com>
2019-04-24 03:04:31 +0300

    Styles for svg paths

--- Web/src/index.js
+++ Web/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Wrapper from './wrapper';
+import './index.css';
 
 ReactDOM.render(<Wrapper />, document.getElementById('root'));
