1a3eae40f4f36c3676d417455c1fbd75d13701c5
Iiro Valkoja <Iiro.Valkoja@Gmail.com>
2019-04-25 03:09:19 +0300

    Initial ip-dialog

--- Web/src/dialog.js
+++ Web/src/dialog.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import {CONNECTING} from './constants';
 
 class Dialog extends React.Component
 {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {'address': this.props.address};
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(aEvent) {
+        this.setState({'address': aEvent.target.value});
+    }
+
+    handleSubmit() {
+        this.props.setConnection(CONNECTING, this.state.address);
+    }
 
     render() {
         return (
-            <div>Dialog</div>
+            <div>
+                <input type="text" value={this.state.address} onChange={this.handleInput} />
+                <button onClick={this.handleSubmit}>Yhdistä</button>
+            </div>
         );
     }
 }
